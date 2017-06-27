8d1e273f49ba2e71d404e59698587daa989f1f21
kijeong kim <kijeong.kim@gmail.com>
2017-06-27 17:43:18 +0900

    [py26] Fix travis pip install unittest2.

--- .travis/install.sh
+++ .travis/install.sh
@@ -11,6 +11,7 @@ pip install check-manifest
 pip install olefile
 pip install pyroma
 pip install coverage
+pip install unittest2
 
 # docs only on Python 2.7
 if [ "$TRAVIS_PYTHON_VERSION" == "2.7" ]; then pip install -r requirements.txt ; fi
