57b74653c4b368e85f35938a17a2a92a633d3dcd
kijeong kim <kijeong.kim@gmail.com>
2017-06-28 11:43:40 +0900

    [py26] Fix a unittest2 install on a docker.

--- .travis/install.sh
+++ .travis/install.sh
@@ -11,7 +11,6 @@ pip install check-manifest
 pip install olefile
 pip install pyroma
 pip install coverage
-pip install unittest2
 
 # docs only on Python 2.7
 if [ "$TRAVIS_PYTHON_VERSION" == "2.7" ]; then pip install -r requirements.txt ; fi
